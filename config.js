5fd75893ac0b2d65c9528bbe580a064fa3f30448
HotelEscueladelosAndesVenezolanos <akuaicomite@gmail.com>
2026-08-02 18:15:37 -0400

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyeo8AtIaoRyI84MU8K3vy4tJwfAJihIuFkBpfk5hKw2BmQ0Ogn4eXlaZX0gMC40W8/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxUEGZiii0LosoovhejFRW5PGBbovXApUz3BteOOe6sn5QOh8bZdydlbsztcrAo9H8/exec"
 };
